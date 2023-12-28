f1b257027692427f9c434feeae7f035eafa084ce
changjun <ckdwns9121@naver.com>
2023-12-29 00:34:18 +0900

    # push

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,36 +1,14 @@
-import "./globals.css";
+import './globals.css';
 
 export const metadata = {
-  title: "WM Farm",
-  description: "안녕하세요. WM Farm입니다.",
+  title: 'WM Farm',
+  description: '안녕하세요. WM Farm입니다.',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <h1>
-          <a href="/">WEB</a>
-        </h1>
-        <ol>
-          <li>
-            <a href="/read/1">about</a>
-            <a href="/read/2">product</a>
-            <a href="/read/3">contact</a>
-          </li>
-        </ol>
-        {children}
-        <ul>
-          <li>
-            <a href="/create">create</a>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <a href="/update">update</a>
-          </li>
-        </ul>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
-import Image from "next/image";
-import Footer from "./Footer";
+import Image from 'next/image';
+import Footer from './Footer';
+import { Flex, Box } from '@chakra-ui/react';
 
 export default function Home() {
   return (
-    <>
+    <div className="container">
       <div className="section1">
-        <div>배경</div>
-        <p>로고</p>
-        <p>더블유엠팜 쇼케이스 식물재배기</p>
-        <p>신선한 도시, 즐거운 농장</p>
+        <div className="logo-wrapper">
+          <Image src={'/logos/header-logo.png'} alt="logo" width={187.5} height={37.5} />
+        </div>
+        <div>
+          <h2>더블유엠팜 쇼케이스 식물재배기</h2>
+          <h1>신선한 도시, 즐거운 농장</h1>
+        </div>
 
         <span>
           <>아이콘</>
@@ -24,61 +28,89 @@ export default function Home() {
         </span>
       </div>
       <div className="selction2">
-        <p>더블유엠팜 쇼케이스 식물재배기</p>
-        <p>다양하게 활용할 수 있습니다</p>
-        <p>직접 식물을 기르며 어디서든 자연을 체험하고 정서적인 안전과 힐링을 느껴보세요</p>
         <div>
-          {/* 사진 부분 */}
-          <div>노인복지관/지자체 image</div>
-          <div>학교/교육기관 image</div>
-          <div>초록 식물이 주는 편안함과 싱그러움으로 어르신들의 정서적 힐링을 선물해드립니다.</div>
-          <div>학교/교육기관에서 관찰 활동을 통해 인성 교육, 정서적인 안정감을 키울 수 있습니다.</div>
-          {/*  */}
-          <div>로비 image</div>
-          <div>레스토랑/카페 image</div>
-          <div>힐링과 여유로움 편안함을 느낄 수 있는 로비 쇼케이스로 초록이 주는 휴식을 느껴보세요</div>
-          <div>쇼케이스로 특별한 레시비/ 고급스러운 인테리어 효과를 경험해 보세요</div>
+          <p>더블유엠팜 쇼케이스 식물재배기</p>
+          <p>다양하게 활용할 수 있습니다</p>
+          <p>직접 식물을 기르며 어디서든 자연을 체험하고 정서적인 안전과 힐링을 느껴보세요</p>
+        </div>
+        <div className="wrapper-image">
+          <div className="flex-image-box">
+            <div className="showcase-item">
+              <img src="/images/section2-1.png" alt="노인복지관" />
+              <div>
+                <strong>학교/교육기관에서 관찰 활동을 통해 인성 교육, 정서적인 안정감을 키울 수 있습니다.</strong>
+              </div>
+            </div>
+            <div className="showcase-item">
+              <img src="/images/section2-1.png" alt="노인복지관" />
+              <div>
+                <strong>학교/교육기관에서 관찰 활동을 통해 인성 교육, 정서적인 안정감을 키울 수 있습니다.</strong>
+              </div>
+            </div>
+          </div>
+          <div className="flex-image-box">
+            <div className="showcase-item">
+              <img src="/images/section2-1.png" alt="노인복지관" />
+              <div>
+                <strong>학교/교육기관에서 관찰 활동을 통해 인성 교육, 정서적인 안정감을 키울 수 있습니다.</strong>
+              </div>
+            </div>
+            <div className="showcase-item">
+              <img src="/images/section2-1.png" alt="노인복지관" />
+              <div>
+                <strong>학교/교육기관에서 관찰 활동을 통해 인성 교육, 정서적인 안정감을 키울 수 있습니다.</strong>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="selction3">
         <div>
-          <span>빛과 물과 온도와 바람까지 알아서 제공해주는 식물재배기 </span>더블유엠팜 쇼케이스
+          <span>빛과 물과 온도와 바람까지 알아서 제공해주는 식물재배기 </span>
+          <h1>더블유엠팜 쇼케이스</h1>
         </div>
-        <div>image</div>
+        <div className="showcase-image-wrapper">
+          <Image src="/images/section3.png" alt="더블유엠팜 쇼케이스" className="showcase-image" layout="fill" />
+        </div>
       </div>
       <div className="section4">
-        <div>더블유엠팜 쇼케이스의 틀별함!</div>
-        <div>image</div>
-        <p>좌, 우측 나눠져 있어</p>
-        <p>온도조절, 물 공기 순환이 가능합니다.</p>
+        <div>
+          <h1>더블유엠팜 쇼케이스의 틀별함!</h1>
+        </div>
+        <div className="section4-image-wrapper">
+          <img src="/images/section4.png" alt="더블유엠팜 쇼케이스" className="showcase-image" />
+          <p>좌, 우측 나눠져 있어</p>
+          <p>온도조절, 물 공기 순환이 가능합니다.</p>
+        </div>
       </div>
       <div className="section5">
         <div>
           <p>인테리어 최적화된 다양한 색상</p>
           <p>디자인도 고급스러워 인테리어 효과 두 배!</p>
         </div>
-        <div>image</div>
+        <img src="/images/section5.png" alt="인테리어 효과 두배" className="showcase-image" />
+
         <p>*해당 색상은 이해를 돕기 위한 것으로 조명의 정도에 따라 색상이 다를 수 있습니다.</p>
       </div>
       <div className="section6">
         <div>더블유엠팜 다른 모델 둘러보기</div>
-        <div>
-          <div>
+        <div className="model-list">
+          <div className="model-item">
             <span>S800모델</span>
-            image
+            <img src="/images/model/s800모델.png" alt="s800" className="model-item-image" />
           </div>
-          <div>
+          <div className="model-item">
             <span>S1400모델</span>
-            image
+            <img src="/images/model/s800모델.png" alt="s800" className="model-item-image" />
           </div>
-          <div>
+          <div className="model-item">
             <span>S1600모델</span>
-            image
+            <img src="/images/model/s800모델.png" alt="s800" className="model-item-image" />
           </div>
         </div>
         <div>구매하기</div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
