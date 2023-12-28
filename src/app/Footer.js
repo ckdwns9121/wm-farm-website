fe1282d0325d7f0a0452f8f01337a16365ca73cb
changjun <ckdwns9121@naver.com>
2023-12-29 03:32:19 +0900

    # 1차

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -1,13 +1,24 @@
-import "./globals.css";
+import './footer.css';
+import Image from 'next/image';
 
 export default function Footer() {
   return (
-    <>
-      <div className="footer-logo"></div>
-      <div>
-        <p>TEL 070-8802-1886</p>
-        <p>E-mail assi2017@naver.com</p>
+    <div className="footer">
+      <div className="footer-item">
+        <div className="footer-logo">
+          <Image src={'/logos/footer-logo.png'} alt="logo" width={187.5} height={37.5} />
+        </div>
       </div>
-    </>
+      <div className="footer-item">
+        <span>
+          <p className="footer-title">TEL </p>
+          <p>070-8802-1886</p>
+        </span>
+        <span>
+          <p className="footer-title">E-mail </p>
+          <p>assi2017@naver.com</p>
+        </span>
+      </div>
+    </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,111 +1,159 @@
 import Image from 'next/image';
 import Footer from './Footer';
-import { Flex, Box } from '@chakra-ui/react';
 
 export default function Home() {
   return (
     <div className="container">
-      <div className="section1">
+      <div className="section1 px-5 py-24 md:px-24">
         <div className="logo-wrapper">
           <Image src={'/logos/header-logo.png'} alt="logo" width={187.5} height={37.5} />
         </div>
         <div>
-          <h2>더블유엠팜 쇼케이스 식물재배기</h2>
-          <h1>신선한 도시, 즐거운 농장</h1>
+          <h2 className="txt-darkgreen text-lg md:text-4xl font-semibold mb-4">더블유엠팜 쇼케이스 식물재배기</h2>
+          <h1 className="txt-blue text-2xl md:text-5xl font-bold"> 신선한 도시, 즐거운 농장</h1>
         </div>
-
-        <span>
-          <>아이콘</>
-          <p>모종과 씨앗 둘 다 생장 가능한 순환식 물 공급 시스템</p>
-        </span>
-        <span>
-          <>아이콘</>
-          <p>최적읜 생육환경 제공 빛, 온동, 물, 바람을 자동으로 공급</p>
-        </span>
-        <span>
-          <>아이콘</>
-          <p>좌/우측 다른 식물 키우기 가능 생육환경을 좌/우측 각각 제어</p>
-        </span>
+        <div className="bottom-icon-wrapper ">
+          <span>
+            <Image src={'/icons/section1-1.png'} alt="icon1" width={30} height={30} />
+            <p className="text-sm md:text-2xl lg-text-3xl font-semibold">
+              모종과 씨앗 둘 다 생장 가능한 순환식 물 공급 시스템
+            </p>
+          </span>
+          <span>
+            <Image src={'/icons/section1-2.png'} alt="icon2" width={30} height={30} />
+            <p className="text-sm md:text-2xl lg-text-3xl font-semibold">
+              최적의 생육환경 제공 빛, 온동, 물, 바람을 자동으로 공급
+            </p>
+          </span>
+          <span>
+            <Image src={'/icons/section1-3.png'} alt="icon2" width={30} height={30} />
+            <p className="text-sm md:text-2xl lg-text-3xl font-semibold">
+              좌/우측 다른 식물 키우기 가능 생육환경을 좌/우측 각각 제어
+            </p>
+          </span>
+        </div>
       </div>
       <div className="selction2">
-        <div>
-          <p>더블유엠팜 쇼케이스 식물재배기</p>
-          <p>다양하게 활용할 수 있습니다</p>
-          <p>직접 식물을 기르며 어디서든 자연을 체험하고 정서적인 안전과 힐링을 느껴보세요</p>
+        <div className="section2-title py-5 px-12 md:py-24 md:px-48">
+          <p className="text-md md:text-3xl font-semibold txt-darkgreen ">더블유엠팜 쇼케이스 식물재배기</p>
+          <p className="text-xl md:text-4xl font-bold txt-blue md:pt-4 pb-8 ">다양하게 활용할 수 있습니다</p>
+          <p className="text-md md:text-2xl font-semibold txt-blue ">
+            직접 식물을 기르며 어디서든 자연을 체험하고 정서적인 안전과 힐링을 느껴보세요
+          </p>
         </div>
         <div className="wrapper-image">
           <div className="flex-image-box">
             <div className="showcase-item">
-              <img src="/images/section2-1.png" alt="노인복지관" />
-              <div>
-                <strong>학교/교육기관에서 관찰 활동을 통해 인성 교육, 정서적인 안정감을 키울 수 있습니다.</strong>
+              <Image src="/images/section2-1.png" alt="노인복지관" width={500} height={500} />
+              <div className="item-sub item-border font-semibold txt-blue text-sm md:text-md p-6 md:p-12 ">
+                <span>초록 식물이 주는 편안함과 싱그러움으로 어르신들의 정서적 힐링을 선물해드립니다</span>
               </div>
             </div>
             <div className="showcase-item">
-              <img src="/images/section2-1.png" alt="노인복지관" />
-              <div>
-                <strong>학교/교육기관에서 관찰 활동을 통해 인성 교육, 정서적인 안정감을 키울 수 있습니다.</strong>
+              <Image src="/images/section2-2.png" alt="노인복지관" width={500} height={500} />
+              <div className="item-sub font-semibold txt-blue text-sm md:text-md  p-6 md:p-12 ">
+                <span>학교/교육기관에서 관찰 활동을 통해 인성 교육, 정서적인 안정감을 키울 수 있습니다.</span>
               </div>
             </div>
           </div>
+
           <div className="flex-image-box">
             <div className="showcase-item">
-              <img src="/images/section2-1.png" alt="노인복지관" />
-              <div>
-                <strong>학교/교육기관에서 관찰 활동을 통해 인성 교육, 정서적인 안정감을 키울 수 있습니다.</strong>
+              <Image src="/images/section2-3.png" alt="노인복지관" width={500} height={500} />
+              <div className="item-sub item-border font-semibold txt-blue text-sm md:text-md p-6 md:p-12">
+                <p>
+                  힐링과 여유로움 편암함을 느낄 수 있는 로비 쇼케이스로
+                  <br />
+                  초록이 주는 휴식을 느껴보세요
+                </p>
               </div>
             </div>
+
             <div className="showcase-item">
-              <img src="/images/section2-1.png" alt="노인복지관" />
-              <div>
-                <strong>학교/교육기관에서 관찰 활동을 통해 인성 교육, 정서적인 안정감을 키울 수 있습니다.</strong>
+              <Image src="/images/section2-4.png" alt="노인복지관" width={500} height={500} />
+              <div className="item-sub font-semibold txt-blue text-sm md:text-md p-6 md:p-12">
+                <span>쇼케이스로 특별한 레시비/고급스러운 인테리어 효과를 경험해보세요</span>
               </div>
             </div>
           </div>
         </div>
       </div>
       <div className="selction3">
-        <div>
-          <span>빛과 물과 온도와 바람까지 알아서 제공해주는 식물재배기 </span>
-          <h1>더블유엠팜 쇼케이스</h1>
+        <div className="bg-green flex-center">
+          <span className="txt-white  text-white text-sm md:text-md lg:text-xl">
+            빛과 물과 온도와 바람까지 알아서 제공해주는 식물재배기
+          </span>
+          <h1 className="txt-blue text-xl md:text-3xl lg:text-4xl font-bold">더블유엠팜 쇼케이스</h1>
         </div>
         <div className="showcase-image-wrapper">
-          <Image src="/images/section3.png" alt="더블유엠팜 쇼케이스" className="showcase-image" layout="fill" />
+          <Image
+            src="/images/section3.png"
+            alt="더블유엠팜 쇼케이스"
+            className="showcase-image"
+            width={500}
+            height={500}
+          />
         </div>
       </div>
       <div className="section4">
-        <div>
-          <h1>더블유엠팜 쇼케이스의 틀별함!</h1>
+        <div className="bg-green flex-center">
+          <h1 className="text-white text-xl md:text-3xl lg:text-4xl font-bold">더블유엠팜 쇼케이스의 특별함</h1>
         </div>
-        <div className="section4-image-wrapper">
-          <img src="/images/section4.png" alt="더블유엠팜 쇼케이스" className="showcase-image" />
-          <p>좌, 우측 나눠져 있어</p>
-          <p>온도조절, 물 공기 순환이 가능합니다.</p>
+        <div className="p-12 md:px-48 lg:px-48">
+          <Image
+            src="/images/section4.png"
+            alt="더블유엠팜 쇼케이스"
+            className="showcase-image"
+            width={500}
+            height={500}
+          />
+          <div
+            className="subscriber 
+          text-lg md:text-2xl
+          font-semibold
+          flex txt-blue text-center justify-center w-full my-10 flex-col"
+          >
+            <p>좌, 우측 나눠져 있어</p>
+            <p className="break-keep">온도조절, 물 공기 순환이 가능합니다.</p>
+          </div>
         </div>
       </div>
-      <div className="section5">
-        <div>
-          <p>인테리어 최적화된 다양한 색상</p>
-          <p>디자인도 고급스러워 인테리어 효과 두 배!</p>
+      <div className="section5 bg-sky  py-4 pb-8 md:py-4 lg:py-4 relative">
+        <div className="flex-center">
+          <p className="text-md md:text-xl lg:text-2xl txt-blue">인테리어 최적화된 다양한 색상</p>
+          <p className="text-lg md:text-2xl lg:text-3xl font-bold txt-blue font-bold">
+            디자인도 고급스러워 인테리어 효과 두 배!
+          </p>
         </div>
-        <img src="/images/section5.png" alt="인테리어 효과 두배" className="showcase-image" />
-
-        <p>*해당 색상은 이해를 돕기 위한 것으로 조명의 정도에 따라 색상이 다를 수 있습니다.</p>
+        <div className="relative">
+          <Image
+            src="/images/section5.png"
+            alt="인테리어 효과 두배"
+            className="image2"
+            width={500}
+            height={500}
+          ></Image>
+          <div className="absolute break-keep bottom-0 md:bottom-4 xl:bottom-4 left-4 font-semibold text-sm pr-4">
+            *해당 색상은 이해를 돕기 위한 것으로 조명의 정도에 따라 색상이 다를 수 있습니다.
+          </div>
+        </div>
       </div>
       <div className="section6">
-        <div>더블유엠팜 다른 모델 둘러보기</div>
-        <div className="model-list">
+        <div className="txt-blue text-xl md:text-3xl lg:text-4xl font-bold py-8 text-center">
+          더블유엠팜 다른 모델 둘러보기
+        </div>
+        <div className="model-list bg-sky px-4 py-8 md:px-8 md: py-12">
           <div className="model-item">
-            <span>S800모델</span>
-            <img src="/images/model/s800모델.png" alt="s800" className="model-item-image" />
+            <span className="text-lg md:text-xl lg:text-2xl font-bold txt-blue"> S800모델</span>
+            <Image src="/images/model/s800모델.png" alt="s800" className="model-item-image" width={500} height={500} />
           </div>
           <div className="model-item">
-            <span>S1400모델</span>
-            <img src="/images/model/s800모델.png" alt="s800" className="model-item-image" />
+            <span className="text-lg md:text-xl lg:text-2xl font-bold txt-blue">S1400모델</span>
+            <Image src="/images/model/s800모델.png" alt="s800" className="model-item-image" width={500} height={500} />
           </div>
           <div className="model-item">
-            <span>S1600모델</span>
-            <img src="/images/model/s800모델.png" alt="s800" className="model-item-image" />
+            <span className="text-lg md:text-xl lg:text-2xl font-bold txt-blue">S1600모델</span>
+            <Image src="/images/model/s800모델.png" alt="s800" className="model-item-image" width={500} height={500} />
           </div>
         </div>
         <div>구매하기</div>
