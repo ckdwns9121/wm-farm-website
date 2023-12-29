75126d4f6e099ff733ea5906fed1d28f6afde4a9
changjun <ckdwns9121@naver.com>
2023-12-29 14:13:09 +0900

    # 최종

--- src/app/page.js
+++ src/app/page.js
@@ -13,33 +13,41 @@ export default function Home() {
     <div className="container">
       <div className="section1 px-5 py-24 md:py-48 md:px-12">
         <div className="logo-wrapper" data-aos="fade-left">
-          <Image src={'/logos/header-logo.png'} alt="logo" width={187.5} height={37.5} />
+          <Image className="logo-icon" src={'/logos/header-logo.png'} alt="logo" width={187.5} height={37.5} />
         </div>
         <div>
-          <h2 className="relative txt-darkgreen text-lg md:text-4xl lg:text-4xl font-semibold mb-4" data-aos="fade-up">
-            더블유엠팜 쇼케이스 식물재배기
-          </h2>
-          <h1 className="txt-blue text-2xl md:text-5xl lg:text-6xl font-bold" data-aos="fade-up">
-            {' '}
-            신선한 도시, 즐거운 농장
-          </h1>
+          <div className="text-background">
+            <div class="blur-background"></div>
+            <h2
+              className="relative txt-darkgreen text-lg md:text-4xl lg:text-4xl font-semibold mb:mb-4"
+              data-aos="fade-up"
+            >
+              더블유엠팜 쇼케이스 식물재배기
+            </h2>
+          </div>
+          <div className="text-background">
+            <div className="blur-background"></div>
+            <h1 className="txt-blue text-2xl md:text-5xl lg:text-6xl font-bold" data-aos="fade-up">
+              신선한 도시, 즐거운 농장
+            </h1>
+          </div>
         </div>
         <div className="bottom-icon-wrapper" data-aos="fade-up">
           <span className="mb-4 md:mb-10">
-            <Image src={'/icons/section1-1.png'} alt="icon1" width={30} height={30} />
-            <p className="text-sm md:text-3xl lg-text-5xl font-semibold">
+            <Image src={'/icons/section1-1.png'} alt="icon1" width={30} height={30} className="section-icon" />
+            <p className="text-xs md:text-3xl lg-text-5xl font-semibold">
               모종과 씨앗 둘 다 생장 가능한 순환식 물 공급 시스템
             </p>
           </span>
           <span className="mb-4 md:mb-10">
-            <Image src={'/icons/section1-2.png'} alt="icon2" width={30} height={30} />
-            <p className="text-sm md:text-3xl lg-text-5xl font-semibold">
+            <Image src={'/icons/section1-2.png'} alt="icon2" width={30} height={30} className="section-icon" />
+            <p className="text-xs md:text-3xl lg-text-5xl font-semibold">
               최적의 생육환경 제공 빛, 온동, 물, 바람을 자동으로 공급
             </p>
           </span>
           <span className="mb-4 md:mb-10">
-            <Image src={'/icons/section1-3.png'} alt="icon2" width={30} height={30} />
-            <p className="text-sm md:text-3xl lg-text-5xl font-semibold">
+            <Image src={'/icons/section1-3.png'} alt="icon2" width={30} height={30} className="section-icon" />
+            <p className="text-xs md:text-3xl lg-text-5xl font-semibold">
               좌/우측 다른 식물 키우기 가능 생육환경을 좌/우측 각각 제어
             </p>
           </span>
@@ -49,7 +57,7 @@ export default function Home() {
         <div className="section2-title py-5 px-12 md:py-24 md:px-48" data-aos="zoom-in">
           <p className="text-base md:text-3xl font-semibold txt-darkgreen ">더블유엠팜 쇼케이스 식물재배기</p>
           <p className="text-xl md:text-5xl font-bold txt-blue md:pt-4 pb-8 ">다양하게 활용할 수 있습니다</p>
-          <p className="text-base md:text-3xl font-semibold txt-blue ">
+          <p className="text-base md:text-3xl font-medium txt-blue ">
             직접 식물을 기르며 어디서든 자연을 체험하고 정서적인 안전과 힐링을 느껴보세요
           </p>
         </div>
@@ -60,11 +68,11 @@ export default function Home() {
                 <Image src="/images/section2-1.png" alt="노인복지관/지자체" width={500} height={500} />
                 <span className="absolute bottom-0 right-0">
                   <span className="badge-container left-badge">
-                    <span className="badge-text text-sm font-bold">노인복지관/지자체</span>
+                    <span className="badge-text text-sm font-semibold">노인복지관/지자체</span>
                   </span>
                 </span>
               </div>
-              <div className="item-sub item-border font-semibold txt-blue text-sm md:text-base p-6 md:p-12 ">
+              <div className="item-sub item-border font-medium txt-blue text-sm md:text-base p-6 md:p-12 ">
                 <span>초록 식물이 주는 편안함과 싱그러움으로 어르신들의 정서적 힐링을 선물해드립니다</span>
               </div>
             </div>
@@ -73,11 +81,11 @@ export default function Home() {
                 <Image src="/images/section2-2.png" alt="학교/교육기관" width={500} height={500} />
                 <div className="absolute left-0 bottom-0">
                   <div className="badge-container ">
-                    <div className="badge-text text-sm font-bold">학교/교육기관</div>
+                    <div className="badge-text text-sm font-semibold">학교/교육기관</div>
                   </div>
                 </div>
               </div>
-              <div className="item-sub font-semibold txt-blue text-sm md:text-base  p-6 md:p-12 ">
+              <div className="item-sub font-medium txt-blue text-sm md:text-base  p-6 md:p-12 ">
                 <span>학교/교육기관에서 관찰 활동을 통해 인성 교육, 정서적인 안정감을 키울 수 있습니다.</span>
               </div>
             </div>
@@ -88,12 +96,12 @@ export default function Home() {
               <div className="relative">
                 <div className="absolute top-0 right-0">
                   <div className="badge-container left-badge">
-                    <div className="badge-text text-sm font-bold">로비</div>
+                    <div className="badge-text text-sm font-semibold">로비</div>
                   </div>
                 </div>
                 <Image src="/images/section2-3.png" alt="로비" width={500} height={500} />
               </div>
-              <div className="item-sub item-border font-semibold txt-blue text-sm md:text-base p-6 md:p-12">
+              <div className="item-sub item-border font-medium txt-blue text-sm md:text-base p-6 md:p-12">
                 <p>
                   힐링과 여유로움 편암함을 느낄 수 있는 로비 쇼케이스로
                   <br />
@@ -106,12 +114,12 @@ export default function Home() {
               <div className="relative">
                 <div className="absolute top-0 left-0">
                   <div className="badge-container ">
-                    <div className="badge-text text-sm font-bold">레스토랑/카페</div>
+                    <div className="badge-text text-sm font-semibold">레스토랑/카페</div>
                   </div>
                 </div>
                 <Image src="/images/section2-4.png" alt="레스토랑/카페" width={500} height={500} />
               </div>
-              <div className="item-sub font-semibold txt-blue text-sm md:text-base p-6 md:p-12">
+              <div className="item-sub font-medium txt-blue text-sm md:text-base p-6 md:p-12">
                 <span>쇼케이스로 특별한 레시비/고급스러운 인테리어 효과를 경험해보세요</span>
               </div>
             </div>
@@ -123,7 +131,7 @@ export default function Home() {
           <span className="txt-white  text-white text-sm md:text-base lg:text-xl" data-aos="fade-up">
             빛과 물과 온도와 바람까지 알아서 제공해주는 식물재배기
           </span>
-          <h1 className="txt-blue text-xl md:text-4xl lg:text-6xl font-bold" data-aos="fade-up">
+          <h1 className="txt-blue text-3xl md:text-4xl lg:text-6xl font-bold" data-aos="fade-up">
             더블유엠팜 쇼케이스
           </h1>
         </div>
@@ -156,7 +164,7 @@ export default function Home() {
           <div
             className="subscriber 
           text-2xl md:text-3xl
-          font-semibold
+          font-medium
           flex txt-blue text-center justify-center w-full my-10 flex-col"
             data-aos="flip-left"
           >
@@ -166,7 +174,7 @@ export default function Home() {
         </div>
       </div>
       <div className="section5 bg-sky  py-6 pb-8 md:py-6 lg:py-6 relative">
-        <div className="flex-center">
+        <div className="text-center">
           <p className="text-base md:text-xl lg:text-4xl txt-blue pb-2" data-aos="fade-up">
             인테리어 최적화된 다양한 색상
           </p>
@@ -189,7 +197,10 @@ export default function Home() {
         </div>
       </div>
       <div className="section6">
-        <div className="txt-blue text-xl md:text-3xl lg:text-5xl font-bold py-12 text-center" data-aos="fade-up">
+        <div
+          className="txt-blue text-xl py-8 md:text-3xl lg:text-5xl font-bold md:py-12 text-center"
+          data-aos="fade-up"
+        >
           더블유엠팜 다른 모델 둘러보기
         </div>
         <div className="model-list bg-sky px-4 py-8 md:px-8 md: py-12">
